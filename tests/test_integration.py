3b9c7839b302daf835c1de4f0fe0f30dbf82d937
dorinbad <dorinidtech@gmail.com>
2025-12-17 16:31:55 +0100

    test: update version expectation to 3.4.1

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -25,7 +25,7 @@ class TestIntegration(unittest.TestCase):
 
     def test_version(self):
         """Test that version is set."""
-        self.assertEqual(VERSION, "3.4.0")
+        self.assertEqual(VERSION, "3.4.1")
 
     def test_constants(self):
         """Test security constants."""
